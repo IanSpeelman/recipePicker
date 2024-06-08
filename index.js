2ba8ec3e3586aae01ef1b5df46bcc71eda91b917
Ian Speelman <ianspeelman@gmail.com>
2024-06-08 14:56:37 +0200

    add routing and templating
    add the route for the recipes
    add ejs and ejs-mate

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express")
 const app = express()
+const engine = require("ejs-mate")
 const port = 3001
+
+app.engine("ejs", engine)
+app.set("views", `${__dirname}/views`)
+app.set("view engine", "ejs")
+
+app.get("/recipes", (req,res) => {
+
+      res.render("recipes/index")
+}) 
 
 app.listen(port, () => console.log(`app listening on port ${port}`))
