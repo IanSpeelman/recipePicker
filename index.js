1e81ee27b5f8fee2d0e86406666dfb21875d7a36
Ian Speelman <ianspeelman@gmail.com>
2024-06-09 11:04:50 +0200

    added the form element and the submit button

--- index.js
+++ index.js
@@ -12,8 +12,9 @@ mongoose.connect("mongodb://localhost:27017/recipe-picker")
       
 app.engine("ejs", engine)
 app.set("views", `${__dirname}/views`)
-app.use(express.static(`${__dirname}/public`))
 app.set("view engine", "ejs")
+app.use(express.static(`${__dirname}/public`))
+app.use(express.urlencoded({extended: true}))
 
 app.get("/recipes", async (req,res) => {
       const recipes = await Recipe.find()
@@ -26,10 +27,17 @@ app.get("/recipes/random", async (req,res) => {
       const recipe = result[rand]
       res.render("recipes/show", { recipe })
 })
+app.get("/recipes/new", (req,res) => {
+      res.render("recipes/new")
+})
 app.get("/recipes/:id", async (req,res) => {
       const { id } = req.params;
       const recipe = await Recipe.findById(id)
       res.render("recipes/show", { recipe })
+})
+app.post("/recipes", (req,res) => {
+      console.log(req.body)
+      res.send(req.body)
 })
 
 app.listen(port, () => console.log(`app listening on port ${port}`))
